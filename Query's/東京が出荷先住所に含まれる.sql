418d53ade9cc6e38b37ee267cfa5770d23467a23
Kimura Taiki <60126875+pokurin123@users.noreply.github.com>
2020-11-14 20:19:40 +0900

    Create 東京が出荷先住所に含まれる.sql

---
+++ Query's/東京が出荷先住所に含まれる.sql
@@ -0,0 +1,15 @@
+SELECT index.商品id,index.商品名,arrived_goods.入荷元住所,delivery.出荷元住所 FROM
+index
+JOIN
+arrived_goods
+ON
+index.商品id=arrived_goods.商品id
+RIGHT JOIN
+delivery
+ON
+index.商品id=delivery.商品id
+WHERE
+delivery.出荷元住所
+LIKE
+'%東京%'
+;
