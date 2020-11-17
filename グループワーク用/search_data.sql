9dcf4875f2be6a3aa3f40f9863edf987844a7cd7
Kimura Taiki <60126875+pokurin123@users.noreply.github.com>
2020-11-17 18:48:34 +0900

    Add files via upload

---
+++ グループワーク用/search_data.sql
@@ -0,0 +1,56 @@
+CREATE TABLE search_data(
+   ID         INTEGER  NOT NULL PRIMARY KEY
+  ,講義名        VARCHAR(14) NOT NULL
+  ,教員名        TEXT NOT NULL
+  ,講義期間1234学期 INTEGER  NOT NULL
+  ,キャンパス      VARCHAR(2) NOT NULL
+  ,対象学年       INTEGER  NOT NULL
+  ,開講時間       VARCHAR(4) NOT NULL
+);
+INSERT INTO search_data(ID,講義名,教員名,講義期間1234学期,キャンパス,対象学年,開講時間) VALUES (1,'英語2A[Aa-1]','朝広 謙次郎',1,'有明',2,'月2');
+INSERT INTO search_data(ID,講義名,教員名,講義期間1234学期,キャンパス,対象学年,開講時間) VALUES (2,'英語2A[Aa-1]','朝広 謙次郎',2,'有明',2,'月2');
+INSERT INTO search_data(ID,講義名,教員名,講義期間1234学期,キャンパス,対象学年,開講時間) VALUES (3,'英語2B[Aa-1]','Tom Saunders',1,'有明',2,'金3');
+INSERT INTO search_data(ID,講義名,教員名,講義期間1234学期,キャンパス,対象学年,開講時間) VALUES (4,'英語2B[Aa-1]','Tom Saunders',2,'有明',2,'金3');
+INSERT INTO search_data(ID,講義名,教員名,講義期間1234学期,キャンパス,対象学年,開講時間) VALUES (5,'データと数理1','中西 崇文  岡田 龍太郎',1,'有明',2,'月3');
+INSERT INTO search_data(ID,講義名,教員名,講義期間1234学期,キャンパス,対象学年,開講時間) VALUES (6,'データと数理1','中西 崇文  岡田 龍太郎',1,'有明',2,'月4');
+INSERT INTO search_data(ID,講義名,教員名,講義期間1234学期,キャンパス,対象学年,開講時間) VALUES (7,'機械学習デザイン演習1','長谷川 理',1,'有明',2,'水1');
+INSERT INTO search_data(ID,講義名,教員名,講義期間1234学期,キャンパス,対象学年,開講時間) VALUES (8,'機械学習デザイン演習1','長谷川 理',1,'有明',2,'水2');
+INSERT INTO search_data(ID,講義名,教員名,講義期間1234学期,キャンパス,対象学年,開講時間) VALUES (9,'社会連携活動概論','上林 憲行',1,'有明',2,'水3');
+INSERT INTO search_data(ID,講義名,教員名,講義期間1234学期,キャンパス,対象学年,開講時間) VALUES (10,'未来創造PJ-A2','個別担当',1,'有明',2,'水4');
+INSERT INTO search_data(ID,講義名,教員名,講義期間1234学期,キャンパス,対象学年,開講時間) VALUES (11,'未来創造PJ-A2','個別担当',1,'有明',2,'水5');
+INSERT INTO search_data(ID,講義名,教員名,講義期間1234学期,キャンパス,対象学年,開講時間) VALUES (12,'未来創造PJ-A2','個別担当',2,'有明',2,'水4');
+INSERT INTO search_data(ID,講義名,教員名,講義期間1234学期,キャンパス,対象学年,開講時間) VALUES (13,'未来創造PJ-A2','個別担当',2,'有明',2,'水5');
+INSERT INTO search_data(ID,講義名,教員名,講義期間1234学期,キャンパス,対象学年,開講時間) VALUES (14,'機械学習デザイン演習2','中西 崇文',2,'有明',2,'月3');
+INSERT INTO search_data(ID,講義名,教員名,講義期間1234学期,キャンパス,対象学年,開講時間) VALUES (15,'機械学習デザイン演習2','中西 崇文',2,'有明',2,'月4');
+INSERT INTO search_data(ID,講義名,教員名,講義期間1234学期,キャンパス,対象学年,開講時間) VALUES (16,'グリーンエコノミー基礎','石橋 直樹',2,'有明',2,'水1');
+INSERT INTO search_data(ID,講義名,教員名,講義期間1234学期,キャンパス,対象学年,開講時間) VALUES (17,'グリーンエコノミー基礎','石橋 直樹',2,'有明',2,'水2');
+INSERT INTO search_data(ID,講義名,教員名,講義期間1234学期,キャンパス,対象学年,開講時間) VALUES (18,'データと数理2','長谷川 理  岡田 龍太郎',2,'有明',2,'金4');
+INSERT INTO search_data(ID,講義名,教員名,講義期間1234学期,キャンパス,対象学年,開講時間) VALUES (19,'データと数理2','長谷川 理  岡田 龍太郎',2,'有明',2,'金5');
+INSERT INTO search_data(ID,講義名,教員名,講義期間1234学期,キャンパス,対象学年,開講時間) VALUES (20,'英語2C[Aa-1]','Vivian Turk',3,'有明',2,'月2');
+INSERT INTO search_data(ID,講義名,教員名,講義期間1234学期,キャンパス,対象学年,開講時間) VALUES (21,'英語2C[Aa-1]','Vivian Turk',4,'有明',2,'月2');
+INSERT INTO search_data(ID,講義名,教員名,講義期間1234学期,キャンパス,対象学年,開講時間) VALUES (22,'英語2D[Aa-1]','Tom Saunders',3,'有明',2,'金3');
+INSERT INTO search_data(ID,講義名,教員名,講義期間1234学期,キャンパス,対象学年,開講時間) VALUES (23,'英語2D[Aa-1]','Tom Saunders',4,'有明',2,'金3');
+INSERT INTO search_data(ID,講義名,教員名,講義期間1234学期,キャンパス,対象学年,開講時間) VALUES (24,'未来創造PJ-A3','個別担当',3,'有明',2,'水4');
+INSERT INTO search_data(ID,講義名,教員名,講義期間1234学期,キャンパス,対象学年,開講時間) VALUES (25,'未来創造PJ-A3','個別担当',3,'有明',2,'水5');
+INSERT INTO search_data(ID,講義名,教員名,講義期間1234学期,キャンパス,対象学年,開講時間) VALUES (26,'未来創造PJ-A3','個別担当',4,'有明',2,'水4');
+INSERT INTO search_data(ID,講義名,教員名,講義期間1234学期,キャンパス,対象学年,開講時間) VALUES (27,'未来創造PJ-A3','個別担当',4,'有明',2,'水5');
+INSERT INTO search_data(ID,講義名,教員名,講義期間1234学期,キャンパス,対象学年,開講時間) VALUES (28,'データと経済統計','長谷川 理',3,'有明',2,'月3');
+INSERT INTO search_data(ID,講義名,教員名,講義期間1234学期,キャンパス,対象学年,開講時間) VALUES (29,'データと経済統計','長谷川 理',3,'有明',2,'月4');
+INSERT INTO search_data(ID,講義名,教員名,講義期間1234学期,キャンパス,対象学年,開講時間) VALUES (30,'マーケティングデータ分析','佐々木 史織',3,'有明',2,'水3');
+INSERT INTO search_data(ID,講義名,教員名,講義期間1234学期,キャンパス,対象学年,開講時間) VALUES (31,'マーケティングデータ分析','佐々木 史織',4,'有明',2,'水3');
+INSERT INTO search_data(ID,講義名,教員名,講義期間1234学期,キャンパス,対象学年,開講時間) VALUES (32,'人工知能AIデザイン演習1','中村 亮太',3,'有明',2,'水1');
+INSERT INTO search_data(ID,講義名,教員名,講義期間1234学期,キャンパス,対象学年,開講時間) VALUES (33,'人工知能AIデザイン演習1','中村 亮太',3,'有明',2,'水2');
+INSERT INTO search_data(ID,講義名,教員名,講義期間1234学期,キャンパス,対象学年,開講時間) VALUES (34,'データと計量経済学','中西 崇文',3,'有明',2,'金4');
+INSERT INTO search_data(ID,講義名,教員名,講義期間1234学期,キャンパス,対象学年,開講時間) VALUES (35,'データと計量経済学','中西 崇文',3,'有明',2,'金5');
+INSERT INTO search_data(ID,講義名,教員名,講義期間1234学期,キャンパス,対象学年,開講時間) VALUES (36,'複合現実','岩田 洋夫',4,'有明',2,'木3');
+INSERT INTO search_data(ID,講義名,教員名,講義期間1234学期,キャンパス,対象学年,開講時間) VALUES (37,'複合現実','岩田 洋夫',4,'有明',2,'木4');
+INSERT INTO search_data(ID,講義名,教員名,講義期間1234学期,キャンパス,対象学年,開講時間) VALUES (38,'機械学習アルゴリズムデザイン','福原 義久',4,'有明',2,'木1');
+INSERT INTO search_data(ID,講義名,教員名,講義期間1234学期,キャンパス,対象学年,開講時間) VALUES (39,'機械学習アルゴリズムデザイン','福原 義久',4,'有明',2,'木2');
+INSERT INTO search_data(ID,講義名,教員名,講義期間1234学期,キャンパス,対象学年,開講時間) VALUES (40,'サイバーフィジカルシステム','上林 憲行',4,'有明',2,'金1');
+INSERT INTO search_data(ID,講義名,教員名,講義期間1234学期,キャンパス,対象学年,開講時間) VALUES (41,'サイバーフィジカルシステム','上林 憲行',4,'有明',2,'金2');
+INSERT INTO search_data(ID,講義名,教員名,講義期間1234学期,キャンパス,対象学年,開講時間) VALUES (42,'人工知能AIデザイン演習2','長谷川 理',4,'有明',2,'金4');
+INSERT INTO search_data(ID,講義名,教員名,講義期間1234学期,キャンパス,対象学年,開講時間) VALUES (43,'人工知能AIデザイン演習2','長谷川 理',4,'有明',2,'金5');
+INSERT INTO search_data(ID,講義名,教員名,講義期間1234学期,キャンパス,対象学年,開講時間) VALUES (44,'人工知能テクノロジ','林 康弘',4,'有明',2,'水1');
+INSERT INTO search_data(ID,講義名,教員名,講義期間1234学期,キャンパス,対象学年,開講時間) VALUES (45,'人工知能テクノロジ','林 康弘',4,'有明',2,'水2');
+INSERT INTO search_data(ID,講義名,教員名,講義期間1234学期,キャンパス,対象学年,開講時間) VALUES (46,'Webテクノロジ','中村 亮太',2,'有明',2,'木1');
+INSERT INTO search_data(ID,講義名,教員名,講義期間1234学期,キャンパス,対象学年,開講時間) VALUES (47,'Webテクノロジ','中村 亮太',2,'有明',2,'木2');
